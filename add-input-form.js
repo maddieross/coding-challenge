8e70840cfe1c7d2c51256b6c8a311068a1830e1c
maddieross <maddieross@u.boisestate.edu>
2019-01-03 01:41:52 -0700

    fixing quotes in js

--- add-input-form.js
+++ add-input-form.js
@@ -1,13 +1,13 @@
 $(function () {
-    var duplicates = 0,
-        $original = $('.user-input').clone(true);
+    
+    $original = $('.user-input').clone(true);
     
     function DuplicateForm () {
         var newForm;
         
         duplicates++; 
 
-        newForm = $original.clone(true).insertBefore($(".add"));
+        newForm = $original.clone(true).insertBefore($('.add'));
         
         $.each($('input', newForm), function(i, item) {            
             $(item).attr('name', $(item).attr('name') + duplicates);
