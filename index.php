78a42261924c8b2e5aad10f2f0014090a63aa7fc
maddieross <maddieross@u.boisestate.edu>
2019-01-03 01:05:15 -0700

    testing form and radio buttons

--- index.php
+++ index.php
@@ -1,10 +1,18 @@
 <html>
     <link href="index.css" type="text/css" rel="stylesheet" />
-    <header><title>Coding Challenge</title></header>
+    <header><title>Paylocity Coding Challenge</title></header>
     <h1><a href="index.php">Coding Challenge</a></h1> 
  
 
     <body>
-        Coding Challenge!
+        <form method="post" action="input_handler.php">
+            <input type="text" id="name" name="name" placeholder="name">
+            <input type="radio" id="employee" name="employee" value="employee"
+                checked>
+            <label for="employee">Huey</label>
+            <input type="radio" id="dpendent" name="dependent" value="dependent"
+                checked>
+            <label for="dependent">Huey</label>
+        </form> 
     </body>
 </html>
