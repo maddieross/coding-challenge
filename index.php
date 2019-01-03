87afe2f667f8c7c56c00e6f239e004a4290181ca
maddieross <maddieross@u.boisestate.edu>
2019-01-03 01:36:24 -0700

    added href for ajax

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <html>
     <link href="index.css" type="text/css" rel="stylesheet" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script type="text/javascript" src="add-input-form.js"></script>
     <header><title>Paylocity Coding Challenge</title></header>
     <h1><a href="index.php">Coding Challenge</a></h1> 
  
