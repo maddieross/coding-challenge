7c54afa8f9e7927e04b58bd95e02878357f67d6e
maddieross <maddieross@u.boisestate.edu>
2019-01-02 20:54:34 -0700

    adding index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+    <link href="index.css" type="text/css" rel="stylesheet" />
+    <header><title>Coding Challenge</title></header>
+    <h1><a href="index.php">Coding Challenge</a></h1> 
+ 
+
+    <body>
+        Coding Challenge!
+    </body>
+</html>
