953b9b6d85b6f4dd1e214af59a85309db303e416
maddieross <maddieross@u.boisestate.edu>
2019-01-03 01:27:54 -0700

    testing javascript add funtction

---
+++ add-input-form.js
@@ -0,0 +1,21 @@
+$(function () {
+    var duplicates = 0,
+        $original = $('.user-input').clone(true);
+    
+    function DuplicateForm () {
+        var newForm;
+        
+        duplicates++; 
+
+        newForm = $original.clone(true).insertBefore($(".add"));
+        
+        $.each($('input', newForm), function(i, item) {            
+            $(item).attr('name', $(item).attr('name') + duplicates);
+        });
+    }
+    
+    $$(".add").click(function(){
+        e.preventDefault();
+        DuplicateForm();
+    });
+});
--- index.php
+++ index.php
@@ -5,15 +5,23 @@
  
 
     <body>
-        <form method="post" action="input_handler.php">
-            <input type="text" id="name" name="name" placeholder="name">
-            <input type="radio" id="employee" name="employee" value="employee"
-                checked>
-            <label for="employee">employee</label>
-            <input type="radio" id="dpendent" name="dependent" value="dependent"
-                checked>
-            <label for="dependent">dependent</label>
-        </form> 
-        <button>+</button>
+        <div class = "user-input">
+            <form method="post" action="input_handler.php">
+                <input type="text" id="name" name="name" placeholder="name">
+                <input type="radio" id="employee" name="employee" value="employee"
+                    checked>
+                <label for="employee">employee</label>
+                <input type="radio" id="dpendent" name="dependent" value="dependent"
+                    checked>
+                <label for="dependent">dependent</label>
+            </form> 
+        </div> 
+        <div class = "add">
+            <button>+</button>
+        </div>
+
+    <div class = "calculate">
+        <button>calculate</button>
+    </div> 
     </body>
 </html>
