25749b3aaff04aaba6e97f73ee91451de60b10a0
maddieross <maddieross@u.boisestate.edu>
2019-01-03 17:40:30 -0700

    trying to print out num of duplicates

--- add-input-form.js
+++ add-input-form.js
@@ -23,7 +23,7 @@ $(document).ready(function () {
         var duplcate = $(this).attr('duplicates')
         $.ajax({
             type: "POST",
-            url: 'calculateHandler.php',
+            url: 'input_handler.php',
             data: {duplicate : duplicate }
         });
     });
