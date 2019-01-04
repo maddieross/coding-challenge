b5c6b405e11958d6eee5547adcc26354a13ae0dc
maddieross <maddieross@u.boisestate.edu>
2019-01-04 13:08:14 -0700

    adding doa

---
+++ Dao.php
@@ -0,0 +1,21 @@
+<?php
+
+class Dao {
+  private $host = "us-cdbr-iron-east-01.cleardb.net";
+  private $db = "heroku_27a5636b1b521da";
+  private $user = "b013e8f8c4be3a";
+  private $pass = "d4ba5526";
+  
+  private function getConnection(){
+    try {
+      $conn = new PDO("mysql:host={$this->host};dbname={$this->db}", $this->user, $this->pass);
+      $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      echo "connection successful";
+      return $conn;
+    } catch (Exception $e) {
+      echo "connection failed: " . $e->getMessage();
+    }
+  }
+
+}
+?>
--- index.php
+++ index.php
@@ -1,30 +1,14 @@
+<?php
+require_once 'Dao.php';
+$dao = new Dao(); 
+?>
 <html>
     <link href="index.css" type="text/css" rel="stylesheet" />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
     <script type="text/javascript" src="add-input-form.js"></script>
     <header><title>Paylocity Coding Challenge</title></header>
     <h1><a href="index.php">Coding Challenge</a></h1> 
- 
-
     <body>
-        <div class = "user-input">
-            <form method="post" action="input_handler.php">
-                <input type="text" id="name" name="name" placeholder="name">
-                <input type="radio" id="employee" name="employee-dependent" value="employee"
-                    checked>
-                <label for="employee">employee</label>
-                <input type="radio" id="dpendent" name="employee-dependent" value="dependent">
-                <label for="dependent">dependent</label>
-            </form> 
-        </div> 
-        <div class = "add">
-            <button>+</button>
-        </div>
-
-        <div class = "calculate">
-            <form method="post" action="input_handler.php">
-            <input type="submit" value="calculate">
-        </div> 
 
     </body>
 </html>
