a2a4f3ea2c087be5af58048fbd61fbf4ec7666f4
maddieross <maddieross@u.boisestate.edu>
2019-01-04 13:30:10 -0700

    checking connection to database

--- Dao.php
+++ Dao.php
@@ -6,12 +6,12 @@ class Dao {
   private $user = "b013e8f8c4be3a";
   private $pass = "d4ba5526";
   
-  private function getConnection(){
+  public function getConnection(){
     try {
       $conn = new PDO("mysql:host={$this->host};dbname={$this->db}", $this->user, $this->pass);
       $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
       echo "connection successful";
-      return $conn;
+      return 'connected';
     } catch (Exception $e) {
       echo "connection failed: " . $e->getMessage();
     }
