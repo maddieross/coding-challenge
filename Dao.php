944db0fa2a1003b068c8e8ab9e1069f4741e4881
maddieross <maddieross@u.boisestate.edu>
2019-01-06 00:09:38 -0700

    fising userID function

--- Dao.php
+++ Dao.php
@@ -49,6 +49,8 @@ class Dao {
   }
 
   private function createEmployeeTable($user_ID){
+    $employee = 'employee';
+    $table_name = $employee.$user_ID; 
     $conn = $this->getConnection();
     $query = $conn->prepare("CREATE TABLE $user_ID (employeeID int, lastName varchar(255), firstName varchar(255), paycheck int, dependents int, deduction int)");
     $query->execute();
