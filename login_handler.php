051d319458aeb165868c4040ec2a852aa6a48918
maddieross <maddieross@u.boisestate.edu>
2019-01-06 15:48:20 -0700

    fixing log in issue

--- login_handler.php
+++ login_handler.php
@@ -3,11 +3,13 @@ session_start();
 
 $login = $_POST['login'];
 $password = $_POST['password'];
-
+echo $login;
+echo $password;
 require_once 'Dao.php';
 $dao = new Dao();
+echo '1';
 $results = $dao->loginIn($login, $passowrd); 
-
+echo '2'; 
 if ($results) {
     $_SESSION['logged_in'] = true;
     $_SESSION['email'] = $login;
@@ -19,7 +21,7 @@ $_SESSION['logged_in'] = false;
 $message = "Username or password invalid";
 $_SESSION['messages'] = $message;
 
-header('Location: login.php');
+header('Location: index.php');
 exit;
 
 ?>
