c34c830a6a622617a8726df86d0dec6d0748739c
maddieross <maddieross@u.boisestate.edu>
2019-01-06 16:09:29 -0700

    trying to find where login funtion in doa is crashing

--- index.php
+++ index.php
@@ -18,8 +18,8 @@
             Log In
             <form method="post" action="login_handler.php">
                 <label for="login">Login:</label><br>
-                <input type="text" id="login" name="login" placeholder="you@example.com"><br>
-                <label for="login">Password:</label><br>
+                <input type="text" id="email" name="email" placeholder="you@example.com"><br>
+                <label for="password">Password:</label><br>
                 <input type="password" id="password" name="password" placeholder="password"><br>
                 <input type="submit" value="Login">
             </form> 
--- login_handler.php
+++ login_handler.php
@@ -1,25 +1,25 @@
 <?php
 session_start();
 
-$login = $_POST['login'];
+$email = $_POST['email'];
 $password = $_POST['password'];
 echo $login;
 echo $password;
 require_once 'Dao.php';
 $dao = new Dao();
 echo '1';
-$results = $dao->loginIn($login, $passowrd); 
+$results = $dao->loginIn($email, $passowrd); 
 echo '2'; 
 if ($results) {
     $_SESSION['logged_in'] = true;
-    $_SESSION['email'] = $login;
+   // $_SESSION['email'] = $login;
     header('Location: account.php');
     exit;
 }
 
 $_SESSION['logged_in'] = false;
 $message = "Username or password invalid";
-$_SESSION['messages'] = $result[0];
+$_SESSION['messages'] = $results[0];
 
 header('Location: index.php');
 exit;
