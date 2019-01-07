ef14d8bc05dbc942ac912ad92c3101ac9ad3f0eb
maddieross <maddieross@u.boisestate.edu>
2019-01-06 17:09:43 -0700

    adding new employee page

--- login_handler.php
+++ login_handler.php
@@ -3,23 +3,19 @@ session_start();
 
 $email = $_POST['email'];
 $password = $_POST['password'];
-echo $login;
-echo $password;
 require_once 'Dao.php';
 $dao = new Dao();
-echo '1';
-$results = $dao->loginIn($email, $password); 
-echo '2'; 
+$results = $dao->loginIn($email, $password);  
 if ($results) {
     $_SESSION['logged_in'] = true;
-   // $_SESSION['email'] = $login;
+    $_SESSION['email'] = $login;
     header('Location: account.php');
     exit;
 }
 
 $_SESSION['logged_in'] = false;
 $message = "Username or password invalid";
-$_SESSION['messages'] = $results[0];
+$_SESSION['messages'] = $messages;
 
 header('Location: index.php');
 exit;
