9389c9b1cb432e2ab8667c976bf826c79485ae7c
maddieross <maddieross@u.boisestate.edu>
2019-01-06 18:43:55 -0700

    trying to locate error in emploee handler

--- employee_handler.php
+++ employee_handler.php
@@ -2,11 +2,15 @@
 session_start();
 
 $first_name = $_POST['first_name'];
+echo $first_name;
 $last_name = $_POST['last_name'];
+echo $last_name;
 $paycheck_amount = $_POST['paycheck_amount'];
 $dependents = $_POST['dependents'];
 $user_ID = $_SESSION['user_ID'];
+echo $user_ID; 
 
+/*
 require_once 'Dao.php';
 $dao = new Dao();
 $results = $dao->newEmployee($user_ID, $first_name, $last_name, $paycheck, $dependents);  
@@ -15,5 +19,5 @@ if($dependents != 0){
     $_SESSION['dependents'] = $dependents;
     echo $first_name[0]; 
 }
-
+*/
 ?>
