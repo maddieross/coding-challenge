056037069c46c6a501e5022a3586380732ff6a4b
maddieross <maddieross@u.boisestate.edu>
2019-01-07 19:39:55 -0700

    trying to print the first letter in a name

--- employee_handler.php
+++ employee_handler.php
@@ -7,16 +7,18 @@ $paycheck_amount = $_POST['paycheck_amount'];
 $dependents = $_POST['dependents'];
 $user_ID = $_SESSION['user_ID'];
 
-
+/*
 require_once 'Dao.php';
 $dao = new Dao();
 $results = $dao->newEmployee($user_ID, $first_name, $last_name, $paycheck, $dependents);  
-echo $results; 
-/*
+
+
 if($dependents != 0){
     $_SESSION['employee_ID'] = $results; 
     $_SESSION['dependents'] = $dependents;
     echo $first_name[0]; 
 }
 */
+echo $first_name[0];
+
 ?>
