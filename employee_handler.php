976fdfea09d840f73119214cf23248ca004820da
maddieross <maddieross@u.boisestate.edu>
2019-01-08 00:33:22 -0700

    trying to add new employee to database with calcualted deduction

--- employee_handler.php
+++ employee_handler.php
@@ -7,21 +7,20 @@ $paycheck_amount = $_POST['paycheck_amount'];
 $dependents = $_POST['dependents'];
 $user_ID = $_SESSION['user_ID'];
 
-/*
+
 require_once 'Dao.php';
 $dao = new Dao();
-$results = $dao->newEmployee($user_ID, $first_name, $last_name, $paycheck, $dependents);  
-
+$employee_ID = $dao->newEmployee($user_ID, $first_name, $last_name, $paycheck, $dependents);  
+echo $employee_ID; 
 
 if($dependents != 0){
     $_SESSION['employee_ID'] = $results; 
-    $_SESSION['dependents'] = $dependents;
+   // header('Location: dependent.php');
+    //exit;
 }
-*/
-if($first_name[M] == 'M'){
-    echo 'true';
-}else{
-    echo 'fales'; 
-}
+
+//header('Location: employees.php.php');
+//exit;
+
 
 ?>
