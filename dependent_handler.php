051be57e7da5e657fe529897a0c6bf26c65b75d0
maddieross <maddieross@u.boisestate.edu>
2019-01-08 19:48:41 -0700

    echoing variables to see if the values are correct

--- dependent_handler.php
+++ dependent_handler.php
@@ -4,6 +4,8 @@ session_start();
 $first_name = $_POST['first_name'];
 $last_name = $_POST['last_name'];
 $user_ID = $_SESSION['user_ID'];
+$employee_ID = $_SESSION['employee_ID'];
+$num_dependents = $_SESSION['num_dependents']-1;
 
 
 require_once 'Dao.php';
@@ -11,11 +13,12 @@ $dao = new Dao();
 $employee_ID = $dao->newDependent($user_ID, $first_name, $last_name);  
 
 
-if($dependents != 0){
-    $_SESSION['employee_ID'] = $results; 
-    $_SESSION['num_dependents'] = $dependents-1; 
-    header('Location: dependent.php');
-    exit;
+if($num_dependents != 0){
+    echo $_SESSION['employee_ID'];
+    echo $user_ID = $_SESSION['user_ID'];
+    echo $num_dependents; 
+   // header('Location: dependent.php');
+    //exit;
 }
 
 header('Location: employees.php.php');
--- employee_handler.php
+++ employee_handler.php
@@ -4,7 +4,7 @@ session_start();
 $first_name = $_POST['first_name'];
 $last_name = $_POST['last_name'];
 $paycheck_amount = $_POST['paycheck_amount'];
-$dependents = $_POST['dependents'];
+$num_dependents = $_POST['dependents'];
 $user_ID = $_SESSION['user_ID'];
 
 
@@ -13,9 +13,9 @@ $dao = new Dao();
 $employee_ID = $dao->newEmployee($user_ID, $first_name, $last_name, $paycheck, $dependents);  
 
 
-if($dependents != 0){
+if($num_dependents != 0){
     $_SESSION['employee_ID'] = $results; 
-    $_SESSION['num_dependents'] = $dependents; 
+    $_SESSION['num_dependents'] = $num_dependents; 
     header('Location: dependent.php');
     exit;
 }
