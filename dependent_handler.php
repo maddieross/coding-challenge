59c37d9c341631b9ec6b4a8ffe811d2ded28d6aa
maddieross <maddieross@u.boisestate.edu>
2019-01-08 19:50:22 -0700

    echoing variables to see if the values are correct

--- dependent_handler.php
+++ dependent_handler.php
@@ -7,11 +7,11 @@ $user_ID = $_SESSION['user_ID'];
 $employee_ID = $_SESSION['employee_ID'];
 $num_dependents = $_SESSION['num_dependents']-1;
 
-
+/*
 require_once 'Dao.php';
 $dao = new Dao();
 $employee_ID = $dao->newDependent($user_ID, $first_name, $last_name);  
-
+*/
 
 if($num_dependents != 0){
     echo $_SESSION['employee_ID'];
