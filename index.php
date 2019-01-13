7f5964e80fca1710befea62d0217cd008af5d96f
maddieross <maddieross@u.boisestate.edu>
2019-01-12 20:38:52 -0700

    trying to center divs

--- index.php
+++ index.php
@@ -17,31 +17,31 @@
             }
             unset($_SESSION['messages']);
         ?>
-        
-        <div class="login">
-            <form method="post" action="login_handler.php">
-                <label for="login">Login</label><br>
-                <input type="text" id="login" name="login" placeholder="you@example.com"><br>
-                <label for="password">Password:</label><br>
-                <input type="password" id="password" name="password" placeholder="password"><br>
-                <input type="submit" value="Login">
-            </form> 
-        </div> 
-        
-        <div class="signup">
-            Sigh Up
-            <form method="post" action="signup_handler.php">
-                <label for="login">Company Name:</label><br>
-                <input type="text" id="name" name="name" placeholder="name"><br>
-                <label for="login">Email:</label><br>
-                <input type="text" id="email" name="email" placeholder="you@example.com"><br>
-                <label for="login">Password:</label><br>
-                <input type="password" id="password" name="password" placeholder="password"><br>
-                <label for="login">Password:</label><br>
-                <input type="password" id="password_check" name="password_check" placeholder="re-enter password"><br>
-                <input type="submit" value="Create Account">
-            </form> 
-        </div> 
+        <div class="center">
+            <div class="login">
+                <form method="post" action="login_handler.php">
+                    <label for="login">Login</label><br>
+                    <input type="text" id="login" name="login" placeholder="you@example.com"><br>
+                    <label for="password">Password:</label><br>
+                    <input type="password" id="password" name="password" placeholder="password"><br>
+                    <input type="submit" value="Login">
+                </form> 
+            </div> 
             
+            <div class="signup">
+                Sigh Up
+                <form method="post" action="signup_handler.php">
+                    <label for="login">Company Name:</label><br>
+                    <input type="text" id="name" name="name" placeholder="name"><br>
+                    <label for="login">Email:</label><br>
+                    <input type="text" id="email" name="email" placeholder="you@example.com"><br>
+                    <label for="login">Password:</label><br>
+                    <input type="password" id="password" name="password" placeholder="password"><br>
+                    <label for="login">Password:</label><br>
+                    <input type="password" id="password_check" name="password_check" placeholder="re-enter password"><br>
+                    <input type="submit" value="Create Account">
+                </form> 
+            </div> 
+        </div> 
     </body> 
 </html>
