b32e9609f711135ac131b5f93d6d8f45c024dfb7
maddieross <maddieross@u.boisestate.edu>
2019-01-15 16:56:36 -0700

    adding delete function for dependent

--- delete_dependent_handler.php
+++ delete_dependent_handler.php
@@ -2,6 +2,7 @@
     session_start();
     require_once 'Dao.php';
     $dao = new Dao();
+    echo $_GET["b"];
     $dao->deleteDependent($_SESSION['ID'], $_GET["a"], $_GET["b"]);
     $_SESSION['messages'] = 'dependent successfully deleted';
     header('Location: edit_employee.php');
