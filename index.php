a9c6a4d80b0f1ef03fc3c2c1db5106f91d676e46
maddieross <maddieross@u.boisestate.edu>
2019-01-14 22:49:05 -0700

    index page has no text background

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <link href="index.css" type="text/css" rel="stylesheet" />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
     <header><title>Paylocity Coding Challenge</title></header>
-    <h1><a href="index.php">Coding Challenge</a></h1> 
+    <h1><a class="index"href="index.php">Coding Challenge</a></h1> 
     <body>
         <?php
             if (isset($_SESSION['messages'])) {
