36cda6a73dfcd34a0e5c6c6b4efe1e752a42bfa8
maddieross <maddieross@u.boisestate.edu>
2019-01-15 15:27:40 -0700

    fixing invalid input handling

--- login_handler.php
+++ login_handler.php
@@ -11,20 +11,20 @@ $password = $_POST['password'];
 require_once 'Dao.php';
 $dao = new Dao();
 $results = $dao->loginIn($email, $password);  
-if ($results) {
-    $_SESSION['logged_in'] = true;
+echo $results;
+if (!$results) {
+    $_SESSION['logged_in'] = false;
+    $messages = "Username or password invalid";
+    $_SESSION['messages'] = $messages;
+    header('Location: index.php');
+    exit;
+}
+
+$_SESSION['logged_in'] = true;
     $_SESSION['ID'] = $results[0];
     $_SESSION['name'] = $results[1];
     $_SESSION['email'] = $results[2];
     header('Location: account.php');
     exit;
-}
-
-$_SESSION['logged_in'] = false;
-$messages = "Username or password invalid";
-$_SESSION['messages'] = $messages;
-
-header('Location: index.php');
-exit;
 
 ?>
