ab1feedba9d0f9c05d9a24de6c760543217d46ca
maddieross <maddieross@u.boisestate.edu>
2019-01-15 17:16:46 -0700

    adding delete function for dependent

--- dependent_handler.php
+++ dependent_handler.php
@@ -5,13 +5,18 @@ $first_name = $_POST['first_name'];
 $last_name = $_POST['last_name'];
 $ID = $_SESSION['ID'];
 $employee_ID = $_SESSION['employee_ID'];
-$dependents = $_SESSION['dependents']-1;
+
+if($_SESSION['dependents']-1){
+    $dependents = $_SESSION['dependents']-1;
+}else{
+    $dependents = 0;
+}
 
 require_once 'Dao.php';
 $dao = new Dao();
 $dao->newDependent($ID, $employee_ID, $first_name, $last_name);  
 
-if($dependents != 0 || !$dependents){
+if($dependents != 0){
     $_SESSION['dependents'] = $dependents; 
     header('Location: dependent.php');
     exit;
