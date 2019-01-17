6e917b231be1f8a6157095231a16edd64af2f178
maddieross <maddieross@u.boisestate.edu>
2019-01-17 16:40:16 -0700

    adding more error messages

--- delete_employee_handler.php
+++ delete_employee_handler.php
@@ -2,7 +2,7 @@
     session_start();
     require_once 'Dao.php';
     $dao = new Dao();
-    if(iset($_GET["a"])){
+    if(isset($_GET["a"])){
         $employee_ID = $_GET["a"];
     }else{
         $employee_ID = $_SESSION['employee_ID'];
