8587132f19f0531f5c60a8e0dcc4ec0d8d08daf1
maddieross <maddieross@u.boisestate.edu>
2019-01-17 15:17:05 -0700

    adding error message display to all pages

--- dependent_handler.php
+++ dependent_handler.php
@@ -6,18 +6,18 @@ $last_name = $_POST['last_name'];
 $ID = $_SESSION['ID'];
 $employee_ID = $_SESSION['employee_ID'];
 
-if($_SESSION['dependents']){
-    $dependents = $_SESSION['dependents']-1;
-}else{
-    $dependents = 0;
-}
-
 if (empty($first_name) || empty($last_name)) {
     $messages = "PLEASE FILL OUT ALL TEXT BOXES";
     $_SESSION['messages'] = $messages;
     $valid = false;
-    header("Location: add_employee.php");
+    header("Location: dependent.php");
     exit;
+}
+
+if($_SESSION['dependents']){
+    $dependents = $_SESSION['dependents']-1;
+}else{
+    $dependents = 0;
 }
 
 require_once 'Dao.php';
